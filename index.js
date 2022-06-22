f3d5b3825531d4fab0b5ac2402e1bd11b92f0ed4
acca3434 <https://github.com/acca3434>
2022-06-22 11:50:29 +0900

    아나

--- index.js
+++ index.js
@@ -71,3 +71,47 @@
 
 // git 저장소가 초기화 된 경로인지 확인 잘하고 사용
 // 경로는 git init을 한 경로
+
+// 1. git 설정
+
+// 사용자 설정
+
+// 깃 사용할때 사용하는 깃의 사용자 세팅
+
+// git config --global user.name "자기 닉네임"
+
+// git config --global user.email "자기 이메일"
+
+// 설정 정보 조회
+// git 설정이 재대로 되었는지 확인
+// git config --global --list
+// git 저장소 초기화 저장소 생성
+// git의 저장소로 지정할 폴더로 이동해서
+// git init
+
+// git 저장소 복제 (파일 내려받기)
+// git clone (url)
+
+// git 저장소 추가
+// git remote add origin "git저장소 url"
+
+// git 사용
+// 스테이징에 추가
+// git add 파일명(확장자 까지)
+
+// 커밋 메시지 
+// git commit -m "작업 내용"
+
+// 현재 브런치 변경
+// git switch "브런치 이름"
+
+// git branch pick
+
+// 브런치 목록 전부보기
+// git branch -r
+
+// 브런치 추가
+// git branch "추가하고 싶은 브런치 이름"
+
+
+
